898429c9e7f00c7f2436e8b7e3cceaf4b07296df
Lars Koopmans <61145612+lkoopmans@users.noreply.github.com>
2022-09-22 09:21:26 +0200

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,7 +1,14 @@
 import subprocess
+import os
 
 # Run requirements
 subprocess.run('pip install -r requirements.txt', shell=True)
+
+if not os.path.exists('videos'):
+    os.mkdir('videos')
+
+if not os.path.exists('images'):
+    os.mkdir('images')
 
 # Run benchmark tests
 subprocess.run('python benchmark_cut_and_merge_files.py', shell=True)
