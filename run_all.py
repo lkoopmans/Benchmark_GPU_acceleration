cdb679faf8fc66b75cb07c6db8748fecdd27cb45
Lars Koopmans <61145612+lkoopmans@users.noreply.github.com>
2022-09-22 09:39:02 +0200

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import torch
 
 # Run requirements
 subprocess.run('pip install -r requirements.txt', shell=True)
@@ -10,8 +11,13 @@ if not os.path.exists('videos'):
 if not os.path.exists('images'):
     os.mkdir('images')
 
-# Run benchmark tests
+print('Cuda available:', torch.cuda.is_available())
+if torch.cuda.is_available():
+    print('Cuda device:', torch.cuda.get_device_name(0))
+
+
+'''# Run benchmark tests
 subprocess.run('python benchmark_cut_and_merge_files.py', shell=True)
 subprocess.run('python benchmark_data_compression.py', shell=True)
 subprocess.run('python benchmark_image_extraction.py', shell=True)
-subprocess.run('python benchmark_object_detection.py', shell=True)
+subprocess.run('python benchmark_object_detection.py', shell=True)'''
